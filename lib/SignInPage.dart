0038bec16f784ea545a37bf762d2dba11ba612e3
MaureenDadap <maureendadap@gmail.com>
2020-11-23 23:34:18 +0800

    1

---
+++ lib/SignInPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class SignInPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _SignInPage();
+
+}
+
+class _SignInPage extends State<SignInPage> {
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+  
+}
--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Twitter Videos Sorter',
       theme: ThemeData(
         primarySwatch: Colors.pink,
         visualDensity: VisualDensity.adaptivePlatformDensity,
